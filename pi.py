e3e70cc01c3bfb8606997601c1c1b74f1565a497
RyoYoshioka-1 <ryo.y1126@icloud.com>
2024-10-15 14:45:07 +0900

    制御構文課題

--- pi.py
+++ pi.py
@@ -1,7 +1,16 @@
+import re
+
 text = """
     How I want a drink, alcoholic of course, after the heavy chapters involving
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
 
-# TODO
+words = text.split()
 
+words = [re.sub(r"[,.]", "", item) for item in words]
+
+words = list(map(len, words))
+
+result = "".join(str(item) for item in words)
+
+print(int(result))
--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,26 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+
+a = int(a)
+b = int(b)
+
+count = 0
+
+for i in range(1, a+1):
+    if a % i ==0:
+        count += 1   
+
+if count == 2:
+    print("{} is prime number!".format(a))
+else:
+    print("{} is not prime number.".format(a))
+
+for i in range(1, b+1):
+    if b % i ==0:
+        count += 1
+
+if count == 2:
+    print("{} is prime number!".format(b))
+else:
+    print("{} is not prime number.".format(b))
