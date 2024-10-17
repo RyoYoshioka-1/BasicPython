a7391911d51de457e49338d9eb68858e05b25de5
RyoYoshioka-1 <ryo.y1126@icloud.com>
2024-10-17 16:07:33 +0900

    制御構造課題

--- euclid.py
+++ euclid.py
@@ -2,22 +2,35 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
-r = 1
 a = int(a)
 b = int(b)
 
-if a > b:
-    while r !=0:
-        r = a % b
-        a = b
-        b = r
-    print("{}が最大公約数です。".format(a))
-elif a < b:
-    while r != 0:
-        r = b % a
-        b = a
-        a = r
-    print("{}が最大公約数です。".format(b))
+
+def euclid(a, b):
+    r = 1
+
+    if a > b:
+        while r !=0:
+            r = a % b
+            a = b
+            b = r
+        return "{}が最大公約数です。".format(a)
+    elif a < b:
+        while r != 0:
+            r = b % a
+            b = a
+            a = r
+        return "{}が最大公約数です。".format(b)
+
+print(euclid(a, b))
+
+def prime():
+    if euclid(a, b) == "1が最大公約数です。":
+        return "さらにこの二数は互いに素です"
+    else:
+        return "しかし、互いに素ではありません"
+
+print(prime())
 
 
 
